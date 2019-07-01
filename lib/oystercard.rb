14c2f899a6a3c5325c83362bf8a455725e309ab1
iAquino47 <ianaquino47@gmail.com>
2019-07-01 15:54:14 +0100

    stage 7 completed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,18 @@
+
 class Oystercard
+  MAXIMUM_BALANCE = 90
   def initialize(balance = 0)
     @balance = balance
   end
+
   attr_reader :balance
+
   def top_up(amount)
+    raise "Error: Maximum balance is £#{MAXIMUM_BALANCE}" if @balance + amount > MAXIMUM_BALANCE
     @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,8 +3,18 @@ describe Oystercard do
   it "initialises with a default balance of 0" do
     expect(subject.balance).to eq 0
   end
+
   it "the balance can be topped up with top_up method" do
     subject.top_up(5)
     expect(subject.balance).to eq 5
+  end
+
+  it "the maximum balance cannot exceed 90" do
+    expect {subject.top_up(91)}.to raise_error("Error: Maximum balance is £#{Oystercard::MAXIMUM_BALANCE}")
+  end
+
+  it "the fare amount gets deducted from the total balance" do
+    subject.top_up(50)
+    expect(subject.deduct(3)).to eq 47
   end
 end
